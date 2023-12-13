6cb948355c398ddc61fdd3047638c8e9937190e2
mukeshr7 <mukeshreddykalathur@gmail.com>
2023-12-13 00:15:07 -0800

    Commit6

--- Mukeshreddy_Kalathur/Phone_Book/models.py
+++ Mukeshreddy_Kalathur/Phone_Book/models.py
@@ -1,10 +1,11 @@
-
 from django.db import models
+from django.utils import timezone
 
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
+    created_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
--- Mukeshreddy_Kalathur/Phone_Book/views.py
+++ Mukeshreddy_Kalathur/Phone_Book/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from .forms import ContactForm
-from django.utils import timezone
 
 def list_contact(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().order_by('-created_time')
     return render(request, 'Phone_Book/list_contact.html', {'contacts': contacts})
 
 def create_contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            contact = form.save(commit=False)
+            contact.save()
             return redirect('Phone_Book:list_contact')
     else:
         form = ContactForm()
